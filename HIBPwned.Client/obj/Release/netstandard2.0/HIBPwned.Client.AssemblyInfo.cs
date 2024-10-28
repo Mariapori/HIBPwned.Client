af9ff75f3bc0d77cdb21fde983d1cbd84fab4a49
Topias Mariapori <topias@mariapori.fi>
2024-10-28 23:32:14 +0200

    inits

--- HIBPwned.Client/obj/Release/netstandard2.0/HIBPwned.Client.AssemblyInfo.cs
+++ HIBPwned.Client/obj/Release/netstandard2.0/HIBPwned.Client.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Topias Mariapori")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+266820266d4c5daf67bcc241f8e5bdd61d1dd85c")]
 [assembly: System.Reflection.AssemblyProductAttribute("HIBPwned.Client")]
